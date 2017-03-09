3d7680d7e8d371a8fbd9661c6a7f309e91e313d0
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2017-03-09 11:26:04 +0100

    Test for runnner node

--- plugin/src/java/nl/esciencecenter/e3dchem/knime/plants/run/RunNodeModel.java
+++ plugin/src/java/nl/esciencecenter/e3dchem/knime/plants/run/RunNodeModel.java
@@ -4,12 +4,15 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 import org.knime.core.data.DataCell;
 import org.knime.core.data.DataColumnSpec;
 import org.knime.core.data.DataColumnSpecCreator;
 import org.knime.core.data.DataRow;
 import org.knime.core.data.DataTableSpec;
+import org.knime.core.data.RowKey;
 import org.knime.core.data.collection.ListCell;
 import org.knime.core.data.def.DefaultRow;
 import org.knime.core.data.def.IntCell;
@@ -54,16 +57,13 @@ public class RunNodeModel extends NodeModel {
 		long currentRow = 0;
 		for (DataRow inRow : inTable) {
 			String mode = ((StringCell) inRow.getCell(modeIndex)).getStringValue();
-			ListCell arguments = ((ListCell) inRow.getCell(argumentsIndex));
-			List<String> commands = new ArrayList<>();
-			commands.add(executable);
-			commands.add("--mode");
-			commands.add(mode);
-			for (DataCell dataCell : arguments) {
-				commands.add(((StringCell) dataCell).getStringValue());
-			}
+			ListCell argumentsCell = ((ListCell) inRow.getCell(argumentsIndex));
+			List<String> arguments = StreamSupport
+					.stream(argumentsCell.spliterator(), false)
+					.map(c -> ((StringCell) c).getStringValue()).collect(Collectors.toList());
 			File workingDirectory = new File(((StringCell) inRow.getCell(workingDirectoryIndex)).getStringValue());
-			process(inRow, commands, workingDirectory, container);
+			DataRow row = process(inRow.getKey(), workingDirectory, executable, mode, arguments);
+			container.addRowToTable(row);
 			exec.checkCanceled();
 			exec.setProgress(0.9 * currentRow / rowCount, " processing row " + currentRow);
 			currentRow++;
@@ -75,13 +75,19 @@ public class RunNodeModel extends NodeModel {
 		return new BufferedDataTable[] { out };
 	}
 
-	private void process(DataRow inRow, List<String> commands, File workingDirectory, BufferedDataContainer container)
+	public DataRow process(RowKey rowKey, File workingDirectory, String executable, String mode, List<String> arguments)
 			throws IOException, InterruptedException {
+		List<String> commands = new ArrayList<>();
+		commands.add(executable);
+		commands.add("--mode");
+		commands.add(mode);
+		for (String argument : arguments) {
+			commands.add(argument);
+		}
 		Process process = new ProcessBuilder(commands).directory(workingDirectory).inheritIO().start();
 		int exitCode = process.waitFor();
 		DataCell cell = new IntCell(exitCode);
-		DataRow row = new DefaultRow(inRow.getKey(), cell);
-		container.addRowToTable(row);
+		return new DefaultRow(rowKey, cell);
 	}
 
 	@Override
---
+++ tests/src/java/nl/esciencecenter/e3dchem/knime/plants/run/RunNodeModelTest.java
@@ -0,0 +1,40 @@
+package nl.esciencecenter.e3dchem.knime.plants.run;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Assume;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.knime.core.data.DataRow;
+import org.knime.core.data.RowKey;
+import org.knime.core.data.def.DefaultRow;
+import org.knime.core.data.def.IntCell;
+
+public class RunNodeModelTest {
+	@Rule
+	public TemporaryFolder folder= new TemporaryFolder();
+
+	@Test
+	public void testProcess() throws IOException, InterruptedException {
+		Assume.assumeTrue(!System.getProperty("os.name").contains("Windows"));
+		
+		RunNodeModel node = new RunNodeModel();
+		RowKey rowKey = RowKey.createRowKey(1234L);
+		File workingDirectory = folder.getRoot();
+		String executable = "/bin/echo";
+		String mode = "bind";
+		List<String> arguments = Arrays.asList("arg1", "arg2");
+
+		DataRow row = node.process(rowKey, workingDirectory, executable, mode, arguments);
+		
+		DefaultRow expected = new DefaultRow(rowKey, new IntCell(0));
+		assertEquals(expected, row);
+	}
+
+}
